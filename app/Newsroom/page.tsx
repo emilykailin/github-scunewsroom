0b10480a543be455f55c2d2088ca68a4fd62334e
Matthew VD <matthewvandyke08@gmail.com>
2025-05-05 23:27:47 -0700

    rough styling to newsroom

--- app/Newsroom/page.tsx
+++ app/Newsroom/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { collection, getDocs, query, orderBy, updateDoc, doc } from 'firebase/firestore';
 import { db, auth } from '../../firebase';
 import Navbar from '@/components/navbar';
+import { Star, Star as StarOutline } from "lucide-react";
 
 export default function NewsroomPage() {
   const [posts, setPosts] = useState<
@@ -52,31 +53,33 @@ export default function NewsroomPage() {
   return (
     <>
       <Navbar />
-      <main className="p-4">
-        <h1 className="text-2xl font-bold mb-4">Newsroom</h1>
-        <div className="space-y-4">
+      <main className="max-w-7xl mx-auto px-4 py-10 bg-white min-h-screen">
+        <h1 className="text-4xl text-black font-bold mb-4">Newsroom</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {posts.map((post) => (
-            <div key={post.id} className="border p-4 rounded">
+            <div key={post.id} className="p-4 rounded bg-gray-100">
               <h2 className="text-xl font-bold">{post.title}</h2>
+              <button
+                onClick={() => toggleStarPost(post.id)}
+                className="py-2 rounded cursor-pointer"
+              >
+                {starredPosts.includes(post.id) ? (
+                  <Star className="h-6 w-6 text-red-500" />
+                ) : (
+                  <StarOutline className="h-6 w-6 text-black-300" />
+                )}
+              </button>
               <p className="text-gray-600">{post.content}</p>
               {post.imageUrl && (
                 <img
                   src={post.imageUrl}
                   alt={post.title}
-                  className="w-1/4 h-auto mt-4"
+                  className="w-full h-auto mt-4"
                 />
               )}
               <p className="text-sm text-gray-500">
                 Posted on {new Date(post.createdAt?.seconds * 1000).toLocaleString()}
               </p>
-              <button
-                onClick={() => toggleStarPost(post.id)}
-                className={`mt-2 px-4 py-2 rounded ${
-                  starredPosts.includes(post.id) ? 'bg-yellow-500' : 'bg-gray-300'
-                }`}
-              >
-                {starredPosts.includes(post.id) ? 'Unstar' : 'Star'}
-              </button>
             </div>
           ))}
         </div>

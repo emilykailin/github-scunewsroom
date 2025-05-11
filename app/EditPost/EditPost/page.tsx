cb93ffb51c6429d001977d2729d5ddab24ad0e86
Matthew VD <matthewvandyke08@gmail.com>
2025-05-10 18:32:12 -0700

    have editpost not push to vercel

--- app/EditPost/EditPost/page.tsx
+++ app/EditPost/EditPost/page.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+export const dynamic = 'force-dynamic'; // <-- Add this to disable static rendering
+
+
 import { useEffect, useState } from 'react';
 import { useRouter, useParams } from 'next/navigation';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
